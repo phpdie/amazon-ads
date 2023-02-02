28bc3ffac23489d8589f7e9ba4dcc78d7c5a4f9e
phpdie <sundaysunsun@vip.qq.com>
2023-02-02 13:57:57 +0800

    预算规则推荐参数为必填项

--- src/SponsoredProducts/BudgetRulesRecommendation.php
+++ src/SponsoredProducts/BudgetRulesRecommendation.php
@@ -16,14 +16,15 @@ class BudgetRulesRecommendation
     /** https://advertising.amazon.com/API/docs/en-us/sponsored-products/3-0/openapi/prod#/BudgetRulesRecommendation/SPGetBudgetRulesRecommendation
      * @return mixed
      */
-    public function index()
+    public function index(array $param)
     {
+        $campaignId = $param['campaignId'];
         $path = '/sp/campaigns/budgetRules/recommendations';
         $headers = [
             'Amazon-Advertising-API-Scope' => $this->instance->getProfileId(),
             'Content-Type' => 'application/vnd.spbudgetrulesrecommendation.v3+json',
             'Accept' => 'application/vnd.spbudgetrulesrecommendation.v3+json',
         ];
-        return $this->instance->sendRequest($path, [], [], 'POST', $headers);
+        return $this->instance->sendRequest($path, $param, [], 'POST', $headers);
     }
 }
