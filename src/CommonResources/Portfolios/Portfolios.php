ce992926cb85b88abbb1f4ffe486bf3e8665b0e3
phpdie <sundaysunsun@vip.qq.com>
2023-02-02 15:22:36 +0800

    完善了Portfolios

--- src/CommonResources/Portfolios/Portfolios.php
+++ src/CommonResources/Portfolios/Portfolios.php
@@ -6,5 +6,56 @@ use AmazonAdsApi\BaseModel;
 
 class Portfolios extends BaseModel
 {
-    
+    /** https://advertising.amazon.com/API/docs/en-us/reference/2/portfolios#/Portfolios/listPortfolios
+     * @return mixed
+     */
+    public function index(array $param = [])
+    {
+        $path = '/v2/portfolios';
+        $headers = [
+            'Amazon-Advertising-API-Scope' => $this->instance->getProfileId(),
+        ];
+        return $this->instance->sendRequest($path, $param, [], 'GET', $headers);
+    }
+
+    /** https://advertising.amazon.com/API/docs/en-us/reference/2/portfolios#/Portfolios/updatePortfolios
+     * @param array $body
+     * @return mixed
+     */
+    public function update(array $body)
+    {
+        $path = '/v2/portfolios';
+        $headers = [
+            'Amazon-Advertising-API-Scope' => $this->instance->getProfileId(),
+        ];
+        return $this->instance->sendRequest($path, [], $body, 'PUT', $headers);
+    }
+
+    /** https://advertising.amazon.com/API/docs/en-us/reference/2/portfolios#/Portfolios/createPortfolios
+     * @param array $body
+     * @return mixed
+     */
+    public function create(array $body)
+    {
+        $path = '/v2/portfolios';
+        $headers = [
+            'Amazon-Advertising-API-Scope' => $this->instance->getProfileId(),
+        ];
+        return $this->instance->sendRequest($path, [], $body, 'POST', $headers);
+    }
+
+    /** https://advertising.amazon.com/API/docs/en-us/reference/2/portfolios#/Portfolios/listPortfolio
+     * @param array $param
+     * @return mixed
+     */
+    public function read(array $param)
+    {
+        $portfolioId = $param['portfolioId'];
+        $path = '/v2/portfolios/{portfolioId}';
+        $path = str_replace('{portfolioId}', $portfolioId, $path);
+        $headers = [
+            'Amazon-Advertising-API-Scope' => $this->instance->getProfileId(),
+        ];
+        return $this->instance->sendRequest($path, [], [], 'GET', $headers);
+    }
 }
--- src/CommonResources/Portfolios/PortfoliosExtended.php
+++ src/CommonResources/Portfolios/PortfoliosExtended.php
@@ -6,5 +6,31 @@ use AmazonAdsApi\AdsRequest;
 
 class PortfoliosExtended extends BaseModel
 {
+    /** https://advertising.amazon.com/API/docs/en-us/reference/2/portfolios#/Portfolios%20extended/listPortfoliosEx
+     * @param array $param
+     * @return mixed
+     */
+    public function index(array $param = [])
+    {
+        $path = '/v2/portfolios/extended';
+        $headers = [
+            'Amazon-Advertising-API-Scope' => $this->instance->getProfileId(),
+        ];
+        return $this->instance->sendRequest($path, $param, [], 'GET', $headers);
+    }
 
+    /** https://advertising.amazon.com/API/docs/en-us/reference/2/portfolios#/Portfolios%20extended/listPortfolioEx
+     * @param array $param
+     * @return mixed
+     */
+    public function read(array $param)
+    {
+        $portfolioId = $param['portfolioId'];
+        $path = '/v2/portfolios/extended/{portfolioId}';
+        $path = str_replace('{portfolioId}', $portfolioId, $path);
+        $headers = [
+            'Amazon-Advertising-API-Scope' => $this->instance->getProfileId(),
+        ];
+        return $this->instance->sendRequest($path, [], [], 'GET', $headers);
+    }
 }
