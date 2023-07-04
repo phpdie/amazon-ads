0c7fed8627a8f8c970899ccbc0291cfcfc7d0891
phpdie <sundaysunsun@vip.qq.com>
2023-07-04 15:08:39 +0800

    upadte ThemeBasedBidRecommendation

--- src/SponsoredProducts/ThemeBasedBidRecommendation.php
+++ src/SponsoredProducts/ThemeBasedBidRecommendation.php
@@ -6,10 +6,12 @@ use AmazonAdsApi\BaseModel;
 
 class ThemeBasedBidRecommendation extends BaseModel
 {
-    /** https://advertising.amazon.com/API/docs/en-us/sponsored-products/3-0/openapi/prod#/ThemeBasedBidRecommendation/GetThemeBasedBidRecommendationForAdGroup_v1
-     * @return mixed
+    /**
+     * https://advertising.amazon.com/API/docs/en-us/sponsored-products/3-0/openapi/prod#/ThemeBasedBidRecommendation/GetThemeBasedBidRecommendationForAdGroup_v1
+     * @param array $body
+     * @return void
      */
-    public function index()
+    public function index(array $body)
     {
         $path = '/sp/targets/bid/recommendations';
         $headers = [
@@ -17,6 +19,6 @@ class ThemeBasedBidRecommendation extends BaseModel
             'Content-Type' => 'application/vnd.spthemebasedbidrecommendation.v3+json',
             'Accept' => 'application/vnd.spthemebasedbidrecommendation.v3+json',
         ];
-        return $this->instance->sendRequest($path, [], [], 'POST', $headers);
+        return $this->instance->sendRequest($path, [], $body, 'POST', $headers);
     }
 }
