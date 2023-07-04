97a7c48b8dd63b515fad5aa8bdbbc41781ede710
phpdie <sundaysunsun@vip.qq.com>
2023-07-04 15:18:05 +0800

    update KeywordTargets

--- src/SponsoredProducts/KeywordTargets.php
+++ src/SponsoredProducts/KeywordTargets.php
@@ -7,9 +7,10 @@ use AmazonAdsApi\BaseModel;
 class KeywordTargets extends BaseModel
 {
     /** https://advertising.amazon.com/API/docs/en-us/sponsored-products/3-0/openapi/prod#/Keyword%20Targets/getRankedKeywordRecommendation
+     * @param array $body
      * @return mixed
      */
-    public function index()
+    public function index(array $body)
     {
         $path = '/sp/targets/keywords/recommendations';
         $headers = [
@@ -17,6 +18,6 @@ class KeywordTargets extends BaseModel
             'Content-Type' => 'application/vnd.spkeywordsrecommendation.v3+json',
             'Accept' => 'application/vnd.spkeywordsrecommendation.v3+json',
         ];
-        return $this->instance->sendRequest($path, [], [], 'POST', $headers);
+        return $this->instance->sendRequest($path, [], $body, 'POST', $headers);
     }
 }
